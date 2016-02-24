e44d44f2a4d74cb9c85d542385c1495c1b513573
John Schweikert <john@schweikert.net>
2016-02-24 15:40:43 -0500

    first version... has problems with maximum iterations

--- racing_sheet.js
+++ racing_sheet.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
+// spreadsheet key is the long id in the sheets URL
 var GoogleSpreadsheet = require('google-spreadsheet');
 var Sheet = new GoogleSpreadsheet('1LI4OBEGMPKxBwtGY_xJ1yT_G-rrCJxl8FaC0q0DaBl0');
 
@@ -8,13 +9,12 @@ var Firebase = require('firebase');
 
 var Moment = require('moment-timezone');
 
-// const TS_PATH = './node_modules/jstrueskill/lib/racingjellyfish/jstrueskill';
-// var TrueSkillCalculator = require(`${TS_PATH}/TrueSkillCalculator`);
-// var GameInfo = require(`${TS_PATH}/GameInfo`).getDefaultGameInfo();
-// var Player = require(`${TS_PATH}/Player`);
-// var Team = require(`${TS_PATH}/Team`);
+const TS_PATH = './node_modules/jstrueskill/lib/racingjellyfish/jstrueskill';
+var TrueSkillCalculator = require(`${TS_PATH}/TrueSkillCalculator`);
+var GameInfo = require(`${TS_PATH}/GameInfo`).getDefaultGameInfo();
+var Player = require(`${TS_PATH}/Player`);
+var Team = require(`${TS_PATH}/Team`);
 
-// spreadsheet key is the long id in the sheets URL
 
 function getSheetRows(sheet, tab) {
   return new Promise((resolve,reject) => {
@@ -40,6 +40,7 @@ function getParticipantMap(rows) {
         firstname: row.first,
         nickname: row.nickname,
       },
+      driverSkill: GameInfo.getDefaultRating()
     });
     return participantMap;
   }, new Map());
@@ -83,105 +84,6 @@ function getLegResults(rows, raceMap) {
   });
   return results;
 }
-
-// function buildRaceTables(rows, participantMap, raceMap) {
-//
-//   let raceTables = new Map();
-//
-//   let results = rows.map((row) => {
-//     return {
-//       raceId: row.raceid,
-//       leg: Number.parseInt(row.leg),
-//       driverId: Number.parseInt(row.driverid),
-//       navigatorId: 'n/a',
-//       startTime: Moment.duration(row.starttime),
-//       endTime: Moment.duration(row.finishtime),
-//       legTime: Moment.duration(row.legduration),
-//       dnf: row.dnf === 'TRUE' ? true : false,
-//       unknownTime: row.unknown === 'TRUE' ? true : false
-//     };
-//   }).sort((r1, r2) => {
-//     if (r1.raceId !== r2.raceId) {
-//       return r1.date - r2.date;
-//     } else if (r1.leg !== r2.leg) {
-//       return r1.leg - r2.leg;
-//     }
-//     let t1 = r1.dnf ? Number.MAX_VALUE : r1.legTime;
-//     let t2 = r2.dnf ? Number.MAX_VALUE : r2.legTime;
-//     return t1 - t2;
-//   }).reduce((results, result) => {
-//     if ((results.length === 0) || (results[length - 1][0].raceId !== result.raceId)) {
-//       results.push(new Array(raceMap.get(result.raceId).numLegs + 1));
-//     }
-//     results[results.length - 1][result.leg].push(result);
-//   }, []);
-//
-//   for (let race of raceMap.values()) {
-//     //TODO: fix this timezone hack
-//     const RACE_DATE = Moment(raceMap.get(race.raceId).date);
-//
-//     let table = results.filter((result) => {
-//       return result.raceId === race.raceId;
-//     }).sort((res1, res2) => {
-//       if (res1.driverId !== res2.driverId) {
-//         return res1.driverId - res2.driverId;
-//       } else { return res1.leg - res2.leg; }
-//     }).filter((result) => {
-//       return result.leg === 1;
-//     }).sort((r1, r2) => {
-//       //TODO: eliminate duplicated code
-//       let t1 = r1.dnf ? Number.MAX_VALUE : r1.legTime;
-//       let t2 = r2.dnf ? Number.MAX_VALUE : r2.legTime;
-//       return t1 - t2;
-//     }).map((result, index, results) => {
-//       return {
-//         driverId: result.driverId,
-//         driverName: participantMap.get(result.driverId).fullname,
-//         navigatorName: 'tbd',
-//         legStartTime: RACE_DATE.clone().add(result.startTime).format(),
-//         legEndTime: RACE_DATE.clone().add(result.endTime).format(),
-//         legElapsedTime: result.legTime.toJSON(),
-//         legRank: result.dnf ? results.length : index + 1
-//       };
-//     });
-//     raceTables.set(race.raceId, table);
-//   }
-//
-//   return raceTables;
-// }
-
-// function scoreLeg(results, participantMap) {
-//   results = results.filter(result => result.unknownTime === false)
-//   .sort((r1, r2) => {
-//     let t1 = r1.dnf ? Number.MAX_VALUE : r1.legTime;
-//     let t2 = r2.dnf ? Number.MAX_VALUE : r2.legTime;
-//     return t1 - t2;
-//   });
-//
-//   //create teams
-//   let teams = results.map(result => {
-//     let driver = new Player(result.driverId);
-//     let driverSkill = participantMap.get(result.driverId).driverSkill;
-//     if (typeof driverSkill === 'undefined') {driverSkill = GameInfo.getDefaultRating();}
-//     return new Team(result.driverId.toString(), driver, driverSkill);
-//   });
-//
-//   //create rankings`
-//   //TODO: change to a map
-//   let finishOrder = [];
-//   results.forEach((result, index) => {
-//     finishOrder.push(result.dnf ? results.length : index + 1);
-//   });
-//
-//   let newSkills = TrueSkillCalculator.calculateNewRatings(GameInfo, teams, finishOrder);
-//
-//   //update scores
-//   teams.forEach(team => {
-//     let player = team.getPlayers()[0];
-//     participantMap.get(player.getId()).driverSkill = newSkills[player];
-//   });
-//
-// }
 
 function resultCompare(r1, r2) {
   let compareVal = (r => {
@@ -320,12 +222,44 @@ function getRaceList(raceResults) {
   return list;
 }
 
-// function getScores(raceResults) {
-//   let scores = Object.keys(raceResults).map(key => {return raceResults[key];})
-//   .map(race => {
-//
-//   })
-// }
+function getScores(raceResults, participantMap) {
+
+  Object.keys(raceResults).map(key => {return raceResults[key];})
+  .forEach(race => {
+    for (let leg = 1; leg <= race.numLegs; leg++) {
+      let results = race.results
+      .filter(result => {return result.unknownTime === false;})
+      .map(result => {
+        return {
+          driverId: result.driverId,
+          rank: result.legs[leg].rank,
+        };
+      })
+      .sort((r1, r2) => {return r1 - r2;})
+      .map(result => {
+        let driver = new Player(result.driverId);
+        let driverSkill = participantMap.get(result.driverId).driverSkill;
+        let team =  new Team(result.driverId.toString(), driver, driverSkill)
+        return [team, result.rank]
+      })
+      .reduce((result, item) => {
+        result.teams.push(item[0]);
+        result.ranks.push(item[1]);
+        return result;
+      }, {teams: [], ranks: []});
+
+      let newSkills = TrueSkillCalculator.calculateNewRatings(GameInfo, results.teams, results.ranks);
+
+      results.teams.forEach(team => {
+        let player = team.getPlayers()[0];
+        participantMap.get(player.getId()).driverSkill = newSkills[player];
+      });
+
+    }
+  });
+
+
+}
 
 function getDriverStats(raceResults, participantMap) {
   let stats = Object.keys(raceResults).map(key => {return raceResults[key].results;})
@@ -369,7 +303,7 @@ function main() {
     let raceResults = getRaceResults(legResults, participantMap, raceMap);
     let raceList = getRaceList(raceResults, raceMap);
 
-    // let driverScores = getScores(raceResults);
+    let driverScores = getScores(raceResults, participantMap);
 
     let driverStats = getDriverStats(raceResults, participantMap);
 
